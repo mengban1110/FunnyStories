b9cd92ae320667684d7681e06b2781e7f749bf8d
SYT <2677615497@qq.com>
2021-03-04 13:19:16 +0800

    更新---配置七牛云

---
+++ src/cn/DoO/Utils/QiNiu/SendInfo.java
@@ -0,0 +1,20 @@
+package cn.DoO.Utils.QiNiu;
+
+import java.util.UUID;
+
+
+public class SendInfo {
+
+	/**
+	 * @desc 返回去掉-的uuid
+	 * @return
+	 */
+	public static String GetUUID() {
+		return UUID.randomUUID().toString().replaceAll("-", "");
+	}
+
+	public static String getTimeMillis() {
+		return System.currentTimeMillis() + "";
+	}
+
+}
