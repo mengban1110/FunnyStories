2ebcb378eab41caf64388f3b4f4b6b5075432c56
mengban1110 <50842998+mengban1110@users.noreply.github.com>
2021-03-04 21:33:03 +0800

    更新---整合代码

---
+++ src/cn/DoO/Background/api/controller/GetinfoController.java
@@ -0,0 +1,47 @@
+package cn.DoO.Background.api.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cn.DoO.Background.api.servlet.InfoServlet;
+import cn.DoO.Utils.NetCode.NetCodeUtils;
+
+/**
+ * 
+* @desc 获取文本数据 
+* 
+* @author TianShuo
+* 
+* @version 2021年3月4日 下午5:16:36
+*/
+
+@SuppressWarnings("serial")
+@WebServlet("/api/home/getinfo")
+public class GetinfoController extends HttpServlet{
+
+	InfoServlet info = new InfoServlet();
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		try 
+		{
+			info.getTextInfo(request, response);
+		} catch (Exception e) 
+		{
+			out.print(NetCodeUtils.errorTomCat());
+		}
+		finally 
+		{
+			out.close();
+		}
+	}
+}
--- src/cn/DoO/Utils/QiNiu/PutFile.java
+++ src/cn/DoO/Utils/QiNiu/PutFile.java
@@ -77,7 +77,7 @@ public class PutFile {
 		System.out.println(Putimgs(byteInputStream, "txt"));
 		System.out.println();
 		
-		
+		 
 	}
 
 }
