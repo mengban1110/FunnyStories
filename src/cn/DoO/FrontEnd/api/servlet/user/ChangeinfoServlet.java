2fc7c298200d8ef0901e204514674b4769cd5cb7
冯天硕 <73507539+bdNianShao@users.noreply.github.com>
2021-04-04 17:33:42 +0800

    更新--完成用户修改数据功能--2021年4月4日17点33分

--- src/cn/DoO/FrontEnd/api/servlet/user/ChangeinfoServlet.java
+++ src/cn/DoO/FrontEnd/api/servlet/user/ChangeinfoServlet.java
@@ -2,6 +2,7 @@ package cn.DoO.FrontEnd.api.servlet.user;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,7 +17,9 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
+import cn.DoO.FrontEnd.api.dao.user.UserDao;
 import cn.DoO.Utils.NetCode.NetCodeUtils;
+import cn.DoO.Utils.QiNiu.PutFile;
 
 /**
 * @desc 修改用户数据
@@ -26,8 +29,8 @@ import cn.DoO.Utils.NetCode.NetCodeUtils;
 * @version 2021年4月4日 下午4:31:03
 */
 public class ChangeinfoServlet {
-
-	public void changeinfo(HttpServletRequest request, HttpServletResponse response) throws IOException, FileUploadException {
+	UserDao userDao = new UserDao();
+	public void changeinfo(HttpServletRequest request, HttpServletResponse response) throws IOException, FileUploadException, ClassNotFoundException, SQLException {
 		PrintWriter out = response.getWriter();
 
 		if (!"POST".equals(request.getMethod())) {
@@ -38,9 +41,12 @@ public class ChangeinfoServlet {
 		Map<String, Object> data = new HashMap<String, Object>();
 		
 		
-		
-		
-		
+		String uid  = null ;
+		String username = null  ;
+		String usersex  = null ;
+		String userbirth  = null ;
+		String usersign  = null ;
+		String token = null ;
 		
 		
 		
@@ -52,7 +58,7 @@ public class ChangeinfoServlet {
 		
 		
 /**
- * =================================处理被头像================================================
+ * =================================处理头像================================================
  */		FileItem imageItem = null;
 		String imagePrifix = "";//图片后缀
 		// 将请求消息实体中每一个项目封装成单独的DiskFileItem(FileItem接口的实现)对象的任务
@@ -84,19 +90,89 @@ public class ChangeinfoServlet {
 						// 仅支持这几种格式的数据
 						imageItem =Item;
 						imagePrifix = prifix;
-						break;
 					}
 					else
 					{
 						print(out, data, "-1", "文件类型不对");
 					}
 				}
+				
+				
+				else {
+					if (Item.getFieldName().equals("token")) {
+						token = Item.getString("utf-8");
+					}
+					if (Item.getFieldName().equals("uid")) {
+						uid = Item.getString("utf-8");
+					}
+					if (Item.getFieldName().equals("username")) {
+						username = Item.getString("utf-8");
+					}
+					if (Item.getFieldName().equals("userbirth")) {
+						userbirth = Item.getString("utf-8");
+					}
+					if (Item.getFieldName().equals("usersign")) {
+						usersign = Item.getString("utf-8");
+					}
+					if (Item.getFieldName().equals("usersex")) {
+						usersex = Item.getString("utf-8");
+					}
+				}
+				
+				
 			}
 		
 		}
 /**
  * ===========================================================================================
  */
+		
+		
+		System.out.println(token);
+		System.out.println(uid);
+		System.out.println(username);
+		System.out.println(usersex);
+		System.out.println(userbirth);
+		System.out.println(usersign);
+		System.out.println(imageItem.getSize());
+/**
+ * ===========================================================================================
+ */	
+		if (
+				token == null || token.equals("")
+				||uid == null || uid.equals("")
+				||username == null || username.equals("")
+				||usersex == null || usersex.equals("")
+				||userbirth == null || userbirth.equals("")
+				||usersign == null || usersign.equals("")
+				||imageItem == null ||imageItem.getSize()<1
+				) 
+		{
+			print(out, data, "-2", "有参数为空不支持修改");
+		}
+		else 
+		{
+			
+			try {
+				
+				
+				Map<String, Object> user = userDao.queryUserByToken(token);
+				if (user == null) 
+				{
+					print(out, data, "-1", "未登录");
+				}
+				else if (uid.equals(user.get("uid").toString())) 
+				{
+					 String useravatar = PutFile.Putimgs(imageItem.getInputStream(), imagePrifix);
+					
+					userDao.updateUser(uid,username,useravatar,usersex,userbirth,usersign);
+					print(out, data, "200", "修改成功");
+				}
+			} catch (Exception e2) {
+				print(out, data, "-2", "有参数错误不支持修改");
+			}
+		}
+		
 		
 }
 	
